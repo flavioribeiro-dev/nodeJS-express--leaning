59d4ef17181b386ab4a8b2094c3fd4452c96d9ba
fribeiro <flavioxconsult@gmail.com>
2026-06-13 15:19:35 -0300

    método POST - adicionando uma nova pessoa a array pessoas

---
+++ src/routes/pessoas.ts
@@ -0,0 +1,31 @@
+import { Router } from "express";
+
+const router = Router();
+
+let pessoas = [
+    {nome: 'Katia', idade: 34},
+    {nome: 'Mauro', idade: 45},
+    {nome: 'Otávio', idade: 14}
+]
+
+router.get('/', (req, res) => {
+    res.status(200).send(pessoas);
+})
+router.get('/:id', (req, res) => {
+    const id = +req.params.id;
+    if(id >=0 && id<pessoas.length) {
+        res.status(200).send(pessoas[id]);
+    } else {
+        res.status(204).send('pessoa não cadastrada');
+    }
+})
+router.post('/', (req, res) => {
+    const novaPessoa = {
+        nome: req.body.nome,
+        idade: req.body.idade
+    };
+    pessoas.push(novaPessoa);
+    res.status(201).send(pessoas)
+})
+
+export default router;
